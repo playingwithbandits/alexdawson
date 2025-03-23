9e694db3c13411e843c53d527227e17c3a3d89d6
Alex Dawson <alexdawson234@hotmail.co.uk>
2025-03-23 12:13:14 +0000

    a

---
+++ src/data/cards2.ts
@@ -0,0 +1,455 @@
+import { Card } from "@/types/game";
+import { cards3 } from "./cards3";
+
+export const cards2: Card[] = [
+  {
+    description:
+      "The person to your left must give you a quick peck on the cheek.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-kiss",
+  },
+  {
+    description: "The person opposite you must pinch your arm for 3 seconds.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-pinch",
+  },
+  {
+    description: "The person to your right must flash their shoulder at you.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-flash",
+  },
+  {
+    description:
+      "The person to your left must give you a quick bite on the shoulder.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-bite",
+  },
+  {
+    description: "The person opposite you must flick your nose 3 times.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-flick",
+  },
+  {
+    description: "The person to your right must rub your arm for 10 seconds.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-rub",
+  },
+  {
+    description:
+      "The person 2 places to your left must kiss your neck for 5 seconds.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-kiss",
+  },
+  {
+    description: "The person opposite you must pinch your thigh for 5 seconds.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-pinch",
+  },
+  {
+    description: "The person to your right must flash their midriff at you.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-flash",
+  },
+  {
+    description:
+      "The person 2 places to your left must bite your neck for 5 seconds.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-bite",
+  },
+  {
+    description: "The person opposite you must flick your ear 5 times.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-flick",
+  },
+  {
+    description:
+      "The person 3 places to your left must kiss your collarbone for 10 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-kiss",
+  },
+  {
+    description:
+      "The person opposite you must pinch your nipple for 5 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-pinch",
+  },
+  {
+    description: "The person to your right must flash their cleavage at you.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-flash",
+  },
+  {
+    description:
+      "The person 2 places to your right must kiss your inner thigh for 15 seconds.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-kiss",
+  },
+  {
+    description:
+      "The person opposite you must pinch and twist your nipple for 10 seconds.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-pinch",
+  },
+  {
+    description: "The person to your right must flash their pussy/dick at you.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-flash",
+  },
+  {
+    description:
+      "The person 3 places to your right must kiss your pussy/dick through your clothes.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-kiss",
+  },
+  {
+    description:
+      "The person opposite you must pinch your clit/dick for 10 seconds.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-pinch",
+  },
+  {
+    description: "The person to your right must flash their asshole at you.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-flash",
+  },
+  {
+    description: "The person to your left must lick your cheek for 3 seconds.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description: "The person opposite you must lick your neck for 5 seconds.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your right must lick your collarbone for 10 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person 2 places to your left must lick your nipple for 10 seconds.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person opposite you must lick your inner thigh for 15 seconds.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your right must lick your pussy/dick through your clothes.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person 3 places to your left must lick your asshole through your clothes.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your left must give you a quick spank on the ass.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-spank",
+  },
+  {
+    description: "The person opposite you must spank your thigh 5 times.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-spank",
+  },
+  {
+    description: "The person to your right must spank your ass 10 times.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-spank",
+  },
+  {
+    description:
+      "The person 2 places to your left must spank your pussy/dick 10 times.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-spank",
+  },
+  {
+    description:
+      "The person opposite you must spank your ass until it turns red.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-spank",
+  },
+  {
+    description:
+      "The person to your right must give you a quick tickle on your sides.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-tickle",
+  },
+  {
+    description:
+      "The person opposite you must tickle your feet for 10 seconds.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-tickle",
+  },
+  {
+    description:
+      "The person to your left must tickle your inner thighs for 15 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-tickle",
+  },
+  {
+    description:
+      "The person 2 places to your right must tickle your pussy/dick through your clothes.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-tickle",
+  },
+  {
+    description:
+      "The person opposite you must tickle your asshole through your clothes.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-tickle",
+  },
+  {
+    description:
+      "The person to your left must give you a quick massage on your shoulders.",
+    penaltyCost: 1,
+    category: "dare",
+    image: "dare-massage",
+  },
+  {
+    description:
+      "The person opposite you must massage your back for 30 seconds.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-massage",
+  },
+  {
+    description:
+      "The person to your right must massage your thighs for 30 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-massage",
+  },
+  {
+    description:
+      "The person 2 places to your left must massage your pussy/dick through your clothes.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-massage",
+  },
+  {
+    description:
+      "The person opposite you must massage your asshole through your clothes.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-massage",
+  },
+  {
+    description:
+      "The person to your left must pour their drink into your mouth, then kiss you to transfer it back.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-drink",
+  },
+  {
+    description:
+      "The person opposite you must take a shot, then kiss you to transfer it to your mouth.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-drink",
+  },
+  {
+    description:
+      "The person to your right must pour their drink into your mouth, then kiss you deeply to transfer it back.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-drink",
+  },
+  {
+    description:
+      "The person 2 places to your left must take a shot, then kiss you passionately to transfer it to your mouth.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-drink",
+  },
+
+  {
+    description:
+      "The person to your left must pour their drink into your mouth, then kiss you passionately to transfer it back.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-drink",
+  },
+  {
+    description:
+      "Give a lap dance to the person on your left. Must be at least 30 seconds.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-dance",
+  },
+  {
+    description:
+      "Strip down to your underwear and do a runway walk through the room.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-clothes",
+  },
+  {
+    description:
+      "Wear a pair of someone else's underwear on your head for the next 3 rounds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-clothes",
+  },
+  {
+    description:
+      "The person to your right must squirt whipped cream on your nipple and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "Do a dramatic reading of your most recent text messages to the group.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-drink",
+  },
+  {
+    description:
+      "The person 3 places to your left must squirt ketchup on your belly button and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your right must draw a smiley face on your butt with permanent marker.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-draw",
+  },
+  {
+    description:
+      "The person 4 places to your left must squirt chocolate sauce on your nipple and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your left must draw a heart on your chest with lipstick.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-draw",
+  },
+  {
+    description:
+      "The person 2 places to your right must squirt honey on your belly button and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person 3 places to your right must draw a star on your forehead with eyeliner.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-draw",
+  },
+  {
+    description:
+      "The person to your left must squirt mustard on your nipple and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person 5 places to your right must draw a flower on your thigh with permanent marker.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-draw",
+  },
+  {
+    description:
+      "The person 4 places to your left must squirt maple syrup on your belly button and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your right must draw a sun on your back with permanent marker.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-draw",
+  },
+  {
+    description:
+      "The person 2 places to your left must squirt BBQ sauce on your nipple and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person 3 places to your right must draw a moon on your cheek with permanent marker.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-draw",
+  },
+  {
+    description:
+      "The person to your left must squirt mayo on your belly button and lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person 4 places to your right must draw a rainbow on your arm with permanent marker.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-draw",
+  },
+  ...cards3,
+];
---
+++ src/data/cards3.ts
@@ -0,0 +1,172 @@
+import { Card } from "@/types/game";
+
+export const cards3: Card[] = [
+  {
+    description:
+      "You have 10 seconds to lick a different body part of each player. Timer resets after each lick.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "Pour their drink into your mouth, then you waterfall it back to them",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person opposite you must take a shot from your mouth, then you take one from theirs.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-drink",
+  },
+  {
+    description:
+      "Drag an ice cube down your body, then let the person opposite you lick it off.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "Use your teeth to drag an ice cube down the person opposite you's belly then lick it off.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "Find a partner, transfer an ice cube from mouth to mouth. The number of swaps is a penalty dished out to the whole group.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must lick your belly button for 10 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "Lick the inner thigh of the person opposite you for 10 seconds.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-lick",
+  },
+  {
+    description:
+      "The person to your left must drag an ice cube down your back, then lick it off.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must put an ice cube in their mouth and kiss you until it melts.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person to your right must drag an ice cube down your inner thigh, then lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person 2 places to your left must put an ice cube in their cleavage, then you must retrieve it with your mouth.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must drag an ice cube down your chest, then lick it off.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person to your right must put an ice cube in their mouth and kiss you passionately until it melts.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person 3 places to your left must drag an ice cube down your stomach, then lick it off.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must put an ice cube in their belly button, then you must retrieve it with your tongue.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person to your left must drag an ice cube down your neck, then lick it off.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person 2 places to your right must put an ice cube in their mouth and kiss you deeply until it melts.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must drag an ice cube down your arm, then lick it off.",
+    penaltyCost: 2,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person to your right must put an ice cube in their cleavage, then you must retrieve it with your tongue.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person 3 places to your left must drag an ice cube down your leg, then lick it off.",
+    penaltyCost: 3,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must put an ice cube in their mouth and kiss you with tongue until it melts.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person to your left must drag an ice cube down your back, then lick it off while kissing your neck.",
+    penaltyCost: 5,
+    category: "dare",
+    image: "dare-ice",
+  },
+  {
+    description:
+      "The person opposite you must give you a hickey on your inner thigh.",
+    penaltyCost: 4,
+    category: "dare",
+    image: "dare-bite",
+  },
+];
