f405892f9742c20f7b4e6d4ffb37d652cbf9d2bd
Alex Dawson <alexdawson234@hotmail.co.uk>
2025-07-15 14:59:41 +0100

    a

--- src/components/horse/rows/PicksRaceRow.tsx
+++ src/components/horse/rows/PicksRaceRow.tsx
@@ -42,9 +42,9 @@ interface PicksRaceRowProps {
   olbgRaceInfoArr: OLBGRaceInfo[] | undefined;
 }
 
-const THRESHOLD = 0.75;
-const THRESHOLD_RATING = 0.85;
-const THRESHOLD_BEATEN_RPR = 0.95;
+const THRESHOLD = 0.7;
+const THRESHOLD_RATING = 0.8;
+const THRESHOLD_BEATEN_RPR = 0.9;
 const DAYS_THRESHOLD_COMFORTABLE = 14;
 const DAYS_THRESHOLD_EYECATCHER = 45;
 const DAYS_THRESHOLD_HAMPERED = 21;
@@ -2918,7 +2918,7 @@ export function PicksRaceRow({
                 title={paraGraph}
                 //highlight1={countGood && sharpRatingGood}
                 highlight4={
-                  //isEwable &&
+                  isEwable &&
                   countGood &&
                   rprBeatenToRaceBeatenAvgDiffGood &&
                   rprBeatenToRaceBeatenAvgDiffMaxGood &&
