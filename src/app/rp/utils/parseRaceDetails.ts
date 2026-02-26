abaf5ab1fc87aeb319deaf1dd0455aab038df507
Alex Dawson <alexdawson234@hotmail.co.uk>
2026-02-26 09:39:23 +0000

    a

--- src/app/rp/utils/parseRaceDetails.ts
+++ src/app/rp/utils/parseRaceDetails.ts
@@ -147,7 +147,7 @@ export async function parseRaceDetails(
   //console.log(`Found ${rows.length} horses to parse`);
   const twoYearsAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 365 * 2);
   const oneYearAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 365);
-  const _180DaysAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 120);
+  const _180DaysAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 180);
   const _60DaysAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 60);
 
   const formValidDateThreshold = _180DaysAgo;
